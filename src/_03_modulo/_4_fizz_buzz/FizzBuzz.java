616b5a4bc795f8eb5aa4a5fdf7c74e0ce359ac7f
Maximillian Ehrath <maximillian.ehrath@jointheleague.org>
2020-04-23 14:06:02 -0700

    Finished.

--- src/_03_modulo/_4_fizz_buzz/FizzBuzz.java
+++ src/_03_modulo/_4_fizz_buzz/FizzBuzz.java
@@ -21,6 +21,21 @@ package _03_modulo._4_fizz_buzz;
 **/
 
 public class FizzBuzz {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("fizzbuzz");
+            }
+            else if (i % 3 == 0) {
+                System.out.println("fizz");
+            }
+            else if (i % 5 == 0) {
+                System.out.println("buzz");
+            }
+            else {
+                System.out.println(i);
+            }
 
+        }
+    }
 }
-
